8c4b414195d339cc8c396ea78237b70e5c6e4fd3
mahdimarino <mahdismahi.13@gmail.com>
2025-02-20 01:27:51 +0400

    add google anaytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,15 @@
 <html lang="en">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H9EBQKQPRM"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-H9EBQKQPRM');
+    </script>
     <script type="text/javascript">
         var _iub = _iub || [];
         _iub.csConfiguration = {
