4b5292ef6e7dc2b55ab9d5104795a5798480a57b
mahdimarino <mahdismahi.13@gmail.com>
2025-08-08 22:29:07 +0400

    951 Old County Road remove this address

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -474,8 +474,8 @@
                                     <li>
                                         <p>Locations</p>
                                         <strong>539 W. Commerce St #6282 <br> Dallas,TX 75208</strong>
-                                        <strong class="mt-2">951 Old County Road,<br> Suite 2-379 <br> Belmont, CA 94002</strong>
-                                        <strong class="mt-2">East Coast HQ - Opening Soon!</strong>
+                                        {{-- <strong class="mt-2">951 Old County Road,<br> Suite 2-379 <br> Belmont, CA 94002</strong>
+                                        <strong class="mt-2">East Coast HQ - Opening Soon!</strong> --}}
                                     </li>
                                     <li>
                                         <p>Email</p>
